e52b792ac032fe07337ced6532328206bc4036f3
Kitson Kelly <me@kitsonkelly.com>
2017-03-17 17:49:12 +0000

    Change iOS version for testing

--- tests/intern-saucelabs.ts
+++ tests/intern-saucelabs.ts
@@ -8,7 +8,7 @@ export const environments = [
 	{ browserName: 'safari', version: '10.0', platform: 'macOS 10.12' },
 	// Andriod causing issues when run from travis
 	// { browserName: 'Browser', appiumVersion: '1.5.3', deviceName: 'Android Emulator', platformVersion: '5.1', platformName: 'Android' },
-	{ browserName: 'iphone', version: '9.2' }
+	{ browserName: 'iphone', version: '9.3' }
 ];
 
 /* SauceLabs supports more max concurrency */
